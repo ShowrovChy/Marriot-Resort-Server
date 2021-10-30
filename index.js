c854a49e93ec85ea131b67a5c96466c18ead1e08
Showrov <ahshowrov1@gmail.com>
2021-10-30 12:24:57 +0600

    everything checked before heroku login

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
-const port = 5000;
+const port = precess.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4wg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
