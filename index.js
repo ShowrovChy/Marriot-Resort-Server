aeee7a46dbda5c4a1c7cfebe1ea69bf6caf44625
Showrov <ahshowrov1@gmail.com>
2021-10-30 23:31:12 +0600

    delete booking add booking and so on

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+// const port = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4wg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,12 +21,11 @@ async function run() {
     await client.connect();
     const database = client.db("disneyResorts");
     const resortCollection = database.collection("resorts");
-    // const bookedResortsCollection = database.collection("bookedResorts");
+    const bookedResortsCollection = database.collection("bookedResorts");
 
     // POST API add resort to database
     app.post("/addResorts", async (req, res) => {
       const resort = req.body;
-      console.log(resort);
       const result = await resortCollection.insertOne(resort);
       res.json(result);
     });
@@ -37,6 +36,42 @@ async function run() {
       const resorts = await cursor.toArray();
       res.json(resorts);
     });
+
+    // POST API  add   resort booking
+
+    app.post("/addBooking", async (req, res) => {
+      const booking = req.body;
+      const result = await bookedResortsCollection.insertOne(booking);
+
+      res.json(result);
+    });
+
+    // GET API get all Booking resorts
+    app.get("/manageAllBookings", async (req, res) => {
+      const cursor = bookedResortsCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // DELETE API   delete booking
+    app.delete("/deleteBooking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookedResortsCollection.deleteOne(query);
+
+      res.json(result);
+    });
+
+    // GET API get only my booking  result
+    app.get("/myBookings/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      const query = { email: email };
+      console.log(query);
+      const result = await bookedResortsCollection.find(query).toArray();
+      res.json(result);
+    });
+
     // GET API get single event
     /*    app.get("/singleEvents/:id", async (req, res) => {
       const id = req.params.id;
