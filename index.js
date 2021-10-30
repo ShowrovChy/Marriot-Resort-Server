6189eef2387b97128fa12311ee1458faed2cbd1e
Showrov <ahshowrov1@gmail.com>
2021-10-30 12:46:45 +0600

    error kheye matha ghorom kicho liikbo na

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
-const port = precess.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i4wg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,6 +17,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    console.log("working");
     await client.connect();
     const database = client.db("disneyResorts");
     const resortCollection = database.collection("resorts");
